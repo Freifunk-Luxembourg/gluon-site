e3623015e56a494e31eebfc45eb78e6894d6e8c1
Georges Herber <georges@dontpanic.lu>
2016-06-30 09:57:53 +0200

    site config for gluon 2016.1.x

--- make-release.sh
+++ make-release.sh
@@ -24,7 +24,7 @@ fi
 
 rm build.log
 rm -r output
-for TARGET in  ar71xx-generic ar71xx-nand brcm2708-bcm2708 brcm2708-bcm2709 mpc85xx-generic ramips-rt305x sunxi x86-64 x86-generic x86-kvm_guest x86-xen_domu
+for TARGET in  ar71xx-generic ar71xx-mikrotik ar71xx-nand brcm2708-bcm2708 brcm2708-bcm2709 mpc85xx-generic ramips-rt305x sunxi x86-64 x86-generic x86-kvm_guest x86-xen_domu
 do
 	if [ -z "$VERSION" ]
 	then
